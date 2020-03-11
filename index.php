54e6be29b4aecc9b7540481b969e14701a6915e6
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-11 15:18:45 +0300

    header

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+
+<main>
+    <div class="container">
+        <div class="header">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <div class="logo">
+                        <img src="img/logo.png" alt="Гипсолит. Штукатурка стен">
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-md-offset-4">
+                <a class = "phone-link" href = "#"> +7 495 771 05 08 </a>
+                </div>
+                <div class="col-md-3">
+                    <div class="wrap">
+                        <div class="wrap-block">
+                            <button class = "button header-btn">
+                                Получить консультацию
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+    </div>
+</main>
+</body>
+</html>
