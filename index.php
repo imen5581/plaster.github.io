7cfd32513b7bdf00912b989905e966bf0c90e00b
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-12 09:54:01 +0300

    initial screen

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!--<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"> -->
+
+</head>
+<body>
+
+<div class="main">
+    <div class="container">
+        <div class="header">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <div class="logo">
+                        <img src="img/logo.png" alt="Гипсолит. Штукатурка стен">
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-md-offset-4">
+                <a class = "phone-link" href = "#"> +7 495 771 05 08 </a>
+                </div>
+                <div class="col-md-3">
+                    <div class="wrap">
+                        <div class="wrap-block">
+                            <button class = "button header-btn">
+                                Получить консультацию
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        <div class="main-content">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="main-title">
+                        <h1>Автоматизированная <br> штукатурка стен</h1>
+                        <p class = "main-subtitle">Идеально ровные стены <br>в кратчайшие сроки</p>
+                        <span>
+                            Cтоимость работ <br>
+                            от <strong>300</strong> руб/м<sup>2</sup>
+                        </span>
+                        <strong class="main-trigger">
+                            Оплата по факту!
+                        </strong>
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-offset-2">
+                    <div class="main-form">
+                        <h2 class="main-form-header">Для расчета точной стоимости <br><strong>закажите выезд замерщика</strong></h2>
+                        <form action="#">
+                            <label for="">Ваше имя</label>
+                            <input type="text" class="main-form-input">
+                            <label for="">Номер телефона</label>
+                            <input type="text" class="main-form-input">
+                            <button class="main-form-btn button" type="submit">Оставьте заявку</button>
+                            <small>Подарок в день обращения</small>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
